e36db2270cefedb6c16fda3422d01f042aadcfd2
987359298@qq.com <myonmyon514>
2023-05-18 15:31:34 +0800

    1

---
+++ event/event1.js
@@ -0,0 +1,13 @@
+﻿var LIM=LIM||{};
+LIM.EVENT=LIM.EVENT||{};
+
+(function(_) {
+    _.true=()=>{return true}
+    _.false=()=>{return false}
+    
+    _.load=()=>{
+        DataManager.loadDatabase()
+        ConfigManager.load()
+    }
+    _.ready=()=>{return DataManager.isDatabaseLoaded()&&Graphics.isFontLoaded('GameFont')}
+})(LIM.EVENT)
---
+++ main.js
@@ -0,0 +1,13 @@
+﻿//=============================================================================
+// main.js
+//=============================================================================
+
+PluginManager.setup($plugins);
+
+window.onload = function() {
+    //SceneManager.run(Scene_Boot);
+    SceneManager.run(LIM.SCENE.Scene,"test");
+    LIM.$data=new LIM.STORAGE.Data()
+    LIM.$bool=new LIM.STORAGE.Bool()
+    LIM.$number=new LIM.STORAGE.Number()
+};
